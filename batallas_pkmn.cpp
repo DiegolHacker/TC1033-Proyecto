6cd911b208a1e62ce93b5a5f9d363d16bfa0f2be
DiegolHacker <111139574+DiegolHacker@users.noreply.github.com>
2022-11-17 23:50:31 -0600

    Batallas Pokemon c++ V1.0.1
    Implementados nuevos pokemon.

--- batallas_pkmn.cpp
+++ batallas_pkmn.cpp
@@ -5,7 +5,32 @@
 
 int main(){
 
-  Pokemon Charizard("Charizard","Fuego","Volador",109,85,78,100);
-  std::cout <<"Vida de Charizard: ";
+  Pokemon Charizard("Charizard","Fuego","Volador",78,109,85,100);
+  std::cout << "\nVida de Charizard: ";
   std::cout << Charizard.get_hp();
+  std::cout << "\nTipo 1: ";
+  std::cout << Charizard.get_tipo1();
+  std::cout << "\nTipo 2: ";
+  std::cout << Charizard.get_tipo2();
+
+  Pokemon Garchomp("Garchomp","Dragon","Tierra",108,130,95,102);
+  std::cout << "\n\nVida de Garchomp: ";
+  std::cout << Garchomp.get_hp();
+  std::cout << "\nTipo 1: ";
+  std::cout << Garchomp.get_tipo1();
+  std::cout << "\nTipo 2: ";
+  std::cout << Garchomp.get_tipo2();
+
+  Pokemon Volcarona("Volcarona","Fuego","Bicho",85,135,105,105);
+  Pokemon Zoroark("Zoroark","Siniestro","",60,120,60,105);
+  Pokemon Lucario("Lucario","Lucha","Acero",70,115,70,90);
+
+  Movimiento Lanzallamas("Lanzallamas","Fuego",95,100);
+  Movimiento Terremoto("Terremoto","Tierra",100,100);
+  Movimiento GarraD("Garra Dragon","Dragon",80,100);
+  Movimiento Vuelo("Vuelo","Volador",90,90);
+  Movimiento AlaAcero("Ala de acero","Acero",90,90);
+  Movimiento Triturar("Triturar","Siniestro",80,100);
+  Movimiento ABocajarro("A bocajarro", "Lucha", 120, 75)
+
 }
