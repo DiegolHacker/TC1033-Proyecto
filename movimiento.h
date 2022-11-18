f6225028cf383fac403acd20388b3ba406f50a8b
DiegolHacker <111139574+DiegolHacker@users.noreply.github.com>
2022-11-17 23:49:54 -0600

    Batallas Pokemon c++ V1.0.1
    Añadidos nuevos getters() y setters()

--- movimiento.h
+++ movimiento.h
@@ -1,12 +1,34 @@
-#include<string>
+#include <string>
 
 class Movimiento{
 private:
   std::string nombre;
+  std::string tipo;
   int potencia;
   int precision;
 
 
 public:
-  Movimiento(std::string nom, int pot, int prec): nombre(nom), potencia(pot), precision(prec){};
+  Movimiento(std::string nom, std::string tip, int pot, int prec): nombre(nom), tipo(tip), potencia(pot), precision(prec){};
+  std::string get_nombre();
+  std::string get_tipo();
+  int get_pot();
+  int get_prec();
 };
+
+
+std::string Movimiento::get_nombre(){
+  return nombre;
+}
+
+std::string Movimiento::get_tipo(){
+  return tipo;
+}
+
+int Movimiento::get_pot(){
+  return potencia;
+}
+
+int Movimiento::get_prec(){
+  return precision;
+}
